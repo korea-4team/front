3837d0cc1febdd141287e4a0bb76ccb27ad1f701
sun <sunyp0713@naver.com>
2023-10-12 16:23:07 +0900

    for merging

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Main from 'views/Main';
+import Footer from 'layouts/Footer';
 
 function App() {
   return (
     <>
     <Main />
+    <Footer />
     </>
   );
 }
