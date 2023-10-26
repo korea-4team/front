4939d63595c2bd3a9fc184ceb0b0a025a5e72288
leejs8041 <leejs8041@naver.com>
2023-10-26 18:12:43 +0900

    merge

--- src/interfaces/response/advertisingBoard/get-advertising-board-location-list.response.dto.ts
+++ src/interfaces/response/advertisingBoard/get-advertising-board-location-list.response.dto.ts
@@ -2,5 +2,5 @@ import ResponseDto from "../response.dto";
 import AdvertisingBoardListResponseDto from "./advertising-board-list.response.dto";
 
 export default interface GetAdvertisingLocationListResponseDto extends ResponseDto{
-  advertisingboardlist : AdvertisingBoardListResponseDto[];
+  advertisingboardList : AdvertisingBoardListResponseDto[];
 }
