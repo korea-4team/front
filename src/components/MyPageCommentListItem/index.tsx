8d2db5315961ba0289c50d74d1cc740221c69d46
sun <sunyp0713@naver.com>
2023-10-31 18:06:17 +0900

    components modified

--- src/components/MyPageCommentListItem/index.tsx
+++ src/components/MyPageCommentListItem/index.tsx
@@ -1,6 +1,8 @@
 import './style.css';
 import { dateFormat } from 'utils';
 import { userCommentList } from 'interfaces/response/mypage/get-my-comment-list.response.dto';
+import { useNavigate } from 'react-router-dom';
+import { REVIEW_BOARD_DETAIL_PATH } from 'constant';
 
 interface Props {
   item: userCommentList;
@@ -8,11 +10,20 @@ interface Props {
 
 export default function MyPageCommentListItem({item}: Props) {
   //          state         //
-  const { writeDatetime, contents, commentNumber, title } = item;
+  const { writeDatetime, contents, commentNumber, title, boardNumber } = item;
+
+  //          function          //
+  // description : 네비게이트함수 //
+  const navigator = useNavigate();
+
+  // event handler //
+  const oncommentClickbutton = () => {
+    navigator(REVIEW_BOARD_DETAIL_PATH(boardNumber));
+  }
 
   //          render          //
   return (
-    <div className="admin-comment-list-item-box">
+    <div className="admin-comment-list-item-box" onClick={oncommentClickbutton}>
       <div className="admin-comment-list-box">
         <div className='admin-comment-item-number'>{commentNumber}</div>
         <div className='admin-comment-item-contents'> {contents} </div>
